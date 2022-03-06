7958a0a8c05fc8808d513adbb8f333526c79b897
Shubham Patel <shubhampatel@Shubhams-MacBook-Pro.local>
2022-03-06 14:32:19 -0500

    added 5 content pages without content

--- app/__init__.py
+++ app/__init__.py
@@ -11,4 +11,24 @@ def create_app():
     def index():
         return render_template('index.html')
 
+    @app.route("/git")
+    def git():
+        return render_template('git.html')
+
+    @app.route("/docker")
+    def docker():
+        return render_template('docker.html')
+
+    @app.route("/flask")
+    def flask():
+        return render_template('flask.html')
+
+    @app.route("/cicd")
+    def cicd():
+        return render_template('cicd.html')
+
+    @app.route("/bootstrap")
+    def bootstrap():
+        return render_template('bootstrap.html')
+
     return app
