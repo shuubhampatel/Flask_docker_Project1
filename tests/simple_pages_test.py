c3c2a23948a151365b2a2d3cbcc2a0c0cb73875b
Shubham Patel <shubhampatel@Shubhams-MacBook-Pro.local>
2022-03-28 01:54:27 -0400

    added content to oops page V1.2 + added pages from Project1 + added tests + modified index with carousel images

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -9,6 +9,10 @@ def test_request_main_menu_links(client):
     assert b'<a href="/aaa" class="nav-link navbarcustomfontcolor">AAA testing</a>' in response.data
     assert b'<a href="/oops" class="nav-link navbarcustomfontcolor">OOPs</a>' in response.data
     assert b'<a href="/solid" class="nav-link navbarcustomfontcolor">SOLID</a>' in response.data
+    assert b'<a class="dropdown-item" href="/git">Git</a>' in response.data
+    assert b'<a class="dropdown-item" href="/docker">Docker</a>' in response.data
+    assert b'<a class="dropdown-item" href="flask">Python/ Flask</a>' in response.data
+    assert b'<a class="dropdown-item" href="cicd">CI/ CD</a>' in response.data
 
 
 def test_request_home(client):
@@ -44,3 +48,31 @@ def test_solid_page(client):
     response = client.get("/solid")
     assert response.status_code == 200
     assert b"Lets read about SOLID" in response.data
+
+
+def test_git_page(client):
+    """This makes the git page"""
+    response = client.get("/git")
+    assert response.status_code == 200
+    assert b"Lets read about Git" in response.data
+
+
+def test_docker_page(client):
+    """This makes the docker page"""
+    response = client.get("/docker")
+    assert response.status_code == 200
+    assert b"Lets read about Docker" in response.data
+
+
+def test_flask_page(client):
+    """This makes the flask page"""
+    response = client.get("/flask")
+    assert response.status_code == 200
+    assert b"Lets read about Python/ Flask" in response.data
+
+
+def test_cicd_page(client):
+    """This makes the ci/cd page"""
+    response = client.get("/cicd")
+    assert response.status_code == 200
+    assert b"Lets read about CI/CD" in response.data
