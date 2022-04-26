1472db886af26d3f554f5ac2cb765333d2edf222
Shubham Patel <shubhampatel@Shubhams-MacBook-Pro.local>
2022-04-26 12:02:06 -0400

    fixed path of log directory

--- app/config.py
+++ app/config.py
@@ -12,7 +12,7 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, '..', DB_DIR, "db2.sqlite")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = os.getenv('UPLOAD_FOLDER', BASE_DIR + '/uploads')
-    LOG_DIR = os.path.join(BASE_DIR, '../logs')
+    LOG_DIR = os.path.join(BASE_DIR, './logs')
 
 
 class ProductionConfig(Config):
